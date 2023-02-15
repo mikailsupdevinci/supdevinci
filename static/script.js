7fc9444a72b826991b044e6d0f92621a518ac6b1
mikailsupdevinci <125256927+mikailsupdevinci@users.noreply.github.com>
2023-02-15 12:28:21 +0100

    Les dossiers static et templates ont été supprimés

--- static/script.js
+++
@@ -1,8 +0,0 @@
-// Attachez un gestionnaire d'événements au bouton de l'API
-$("#bouton-api").click(function() {
-  // Utilisez jQuery pour appeler l'API
-  $.getJSON("/appeler-api", function(data) {
-    // Insérez les données de l'API dans le div de résultat
-    $("#resultat-api").html(JSON.stringify(data));
-  });
-});
